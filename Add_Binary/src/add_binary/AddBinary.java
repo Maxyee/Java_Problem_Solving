856e7d899dd4a73652e0e07fccc2f136422f2a6d
maxyee <eyaminkhan00@gmail.com>
2024-07-06 19:06:11 +0600

    solved the problem for adding two binary digits

---
+++ Add_Binary/src/add_binary/AddBinary.java
@@ -0,0 +1,29 @@
+package add_binary;
+
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while(i >=0 || j>=0 || carry == 1){
+            if (i>=0){
+                carry = carry + a.charAt(i--) - '0';
+            }
+            if (j>=0){
+                carry = carry + b.charAt(j--) - '0';
+            }
+            sb.append(carry % 2);
+            carry = carry / 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args){
+        String a = "11";
+        String b = "1";
+        String result = addBinary(a,b);
+        System.out.println(result);
+    }
+}
